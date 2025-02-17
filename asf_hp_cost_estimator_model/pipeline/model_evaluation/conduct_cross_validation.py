fbaf7e4d1a76c0060c5457bfcf2f7201af2365b2
sofiapinto <anasbpinto3@gmail.com>
2025-02-17 13:22:29 +0000

    adding documentation to corss validation pipeline

--- asf_hp_cost_estimator_model/pipeline/model_evaluation/conduct_cross_validation.py
+++ asf_hp_cost_estimator_model/pipeline/model_evaluation/conduct_cross_validation.py
@@ -10,13 +10,14 @@ from sklearn.inspection import (
     PartialDependenceDisplay,
 )  # required to be imported before IterativeImputer
 import logging
-from typing import List
+from typing import Tuple, List
 import os
 from sklearn.metrics import (
     mean_absolute_error,
     median_absolute_error,
 )
 import datetime as dt
+from sklearn.pipeline import Pipeline
 
 # local imports
 from asf_hp_cost_estimator_model import config
@@ -176,8 +177,24 @@ def update_error_results_by_dummy_group(
 
 
 def fit_model(
-    model_data, X_train, y_train, date_double_weights=config["date_double_weights"]
-):
+    model_data: Pipeline,
+    X_train: pd.DataFrame,
+    y_train: np.array,
+    date_double_weights: str = config["date_double_weights"],
+) -> Pipeline:
+    """
+    Fit the model on the training data.
+
+    Args:
+        model_data (Pipeline): model data
+        X_train (pd.DataFrame): training data
+        y_train (np.array): training target
+        date_double_weights (str, optional): date from when we double the weights. Defaults to config["date_double_weights"].
+
+    Returns:
+        Pipeline: fitted model pipeline
+    """
+
     # To codify increased reliability in data after a certain date double the weight of the samples
     train_weights = np.where(
         model_data.loc[X_train.index]["commission_date"] >= date_double_weights,
@@ -194,16 +211,29 @@ def fit_model(
 
 def perform_kfold_cross_validation(
     model_data: pd.DataFrame,
-    numeric_features,
+    numeric_features: List[str],
     categorical_features: List[str],
-    target_feature,
+    target_feature: str,
     kfold_splits: int,
-):
+) -> Tuple[List, List, dict]:
+    """
+    Performs k-fold cross-validation.
 
-    #
+    Args:
+        model_data (pd.DataFrame): _description_
+        numeric_features (List[str]): _description_
+        categorical_features (List[str]): _description_
+        target_feature (str): _description_
+        kfold_splits (int): _description_
+
+    Returns:
+        Tuple[List, List, dict]: _description_
+    """
+    # Define input and target data
     X = model_data[numeric_features + categorical_features]
     y = model_data[target_feature].values.ravel()
 
+    # Define a K-Fold cross-validator
     kf = KFold(n_splits=kfold_splits, shuffle=True, random_state=0)
 
     # Initialise result variables
@@ -228,21 +258,27 @@ def perform_kfold_cross_validation(
             )
             first_fold = False
 
+        # Fit the model
         model = fit_model(model_data, X_train, y_train)
 
+        # Predict on the test set
         y_test_pred = model.predict(X_test)
 
+        # Predict with a constant model (based on the mean of the training set)
+        # this is used to assess how good the model is compared to a simple model
         y_test_pred_constant = np.full_like(y_test, np.mean(y_train))
 
+        # Calculate the proportion of training data after a fixed date
         after_date = model_data[
             (model_data["commission_date"] >= config["date_double_weights"])
         ]
         after_date_train = after_date[after_date.index.isin(X_train.index)]
         proportion_train_after_date = len(after_date_train) / len(X_train)
 
-        # np.where always returns a tuple, so we need to extract the first element
+        # Calculate the proportion of testing data after a fixed date
         after_date_test = np.where(X_test.index.isin(after_date.index), True, False)
 
+        # Update the results of the constant model with fold specific results
         results_constant = update_error_results(
             results_constant,
             y_test,
@@ -250,6 +286,8 @@ def perform_kfold_cross_validation(
             proportion_train_after_date,
             after_date_test,
         )
+
+        # Update the results of the model with fold specific results
         results_model = update_error_results(
             results_model,
             y_test,
@@ -257,6 +295,8 @@ def perform_kfold_cross_validation(
             proportion_train_after_date,
             after_date_test,
         )
+
+        # Update the results of the model with fold specific results for each dummy group
         results_per_categorical_feature = update_error_results_by_dummy_group(
             model_data,
             test_index,
@@ -269,7 +309,19 @@ def perform_kfold_cross_validation(
     return results_constant, results_model, results_per_categorical_feature
 
 
-def compare_average_to_model(results_constant, results_model):
+def compare_average_to_model(
+    results_constant: List[dict], results_model: List[dict]
+) -> pd.DataFrame:
+    """
+    Compares the results of the model to the constant model.
+
+    Args:
+        results_constant (List[dict]): List of dictionaries containing the results of the constant model evaluation
+        results_model (List[dict]): List of dictionaries containing the results of the model evaluation
+
+    Returns:
+        pd.DataFrame: Dataframe with the average results of the model and the constant model
+    """
     results_model = pd.DataFrame(results_model).mean(axis=0).round(2)
 
     results_constant = pd.DataFrame(results_constant).mean(axis=0).round(2)
@@ -280,7 +332,16 @@ def compare_average_to_model(results_constant, results_model):
     return results
 
 
-def summarise_results_categories(results_per_categorical_feature):
+def summarise_results_categories(results_per_categorical_feature: dict) -> pd.DataFrame:
+    """
+    Summarises the results of the model evaluation for ach dummy feature.
+
+    Args:
+        results_per_categorical_feature (dict): dictionary containing the results of the model evaluation for each dummy feature
+
+    Returns:
+        pd.DataFrame: Dataframe with the average results of the model for each dummy feature
+    """
     results_categories = pd.DataFrame()
     for feature in categorical_features:
         aux = (
@@ -298,18 +359,19 @@ if __name__ == "__main__":
 
     logging.info("Number of samples: " + str(len(model_data)))
 
+    # Perform k-fold cross-validation
     results_constant, results_model, results_per_categorical_feature = (
         perform_kfold_cross_validation(model_data, categorical_features, kfold_splits)
     )
 
+    # Summarise results from k-fold cross-validation
     overall_results = compare_average_to_model(results_constant, results_model)
+    results_per_category = summarise_results_categories(results_per_categorical_feature)
 
+    # Save results
     overall_results.to_csv(
         os.path.join(model_evaluation_folder, "cross_validation_results.csv")
     )
-
-    results_per_category = summarise_results_categories(results_per_categorical_feature)
-
     results_per_category.to_csv(
         os.path.join(
             model_evaluation_folder, "cross_validation_categorical_feature_results.csv"
