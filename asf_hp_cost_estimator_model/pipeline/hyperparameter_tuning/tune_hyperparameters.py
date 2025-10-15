b317ae79f7e4c618d6eaf4c7c1b6e7fa1c456c3c
Sofia Pinto <70645959+sofiapinto@users.noreply.github.com>
2025-10-15 11:13:53 +0100

    Update asf_hp_cost_estimator_model/pipeline/hyperparameter_tuning/tune_hyperparameters.py
    Co-authored-by: crispy-wonton <104171770+crispy-wonton@users.noreply.github.com>

--- asf_hp_cost_estimator_model/pipeline/hyperparameter_tuning/tune_hyperparameters.py
+++ asf_hp_cost_estimator_model/pipeline/hyperparameter_tuning/tune_hyperparameters.py
@@ -8,7 +8,7 @@ The data is split into training and two hold-out test sets. One test set is a 20
 Hyperparameter tuning is performed using Halving Random Search with cross-validation on the training set.
 
 After tuning the models, they are evaluated on the training, test set and most recent quarter hold-out test set.
-The script loggs various metrics such as mean pinball loss, coverage probability, and interval widths.
+The script logs various metrics such as mean pinball loss, coverage probability, and interval widths.
 
 The best hyperparameters and evaluation metrics are then saved as CSV files to S3.
 
